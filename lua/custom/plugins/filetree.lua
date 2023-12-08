ed88efb388e9c116bf9983646f2c1ef5851a727f
danieljonguitud <daniel.jonguitud@sayvero.com>
2023-12-08 08:24:37 -0800

    hidding dot files on neotree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -16,8 +16,8 @@ return {
       close_if_last_window = true,
       filesystem = {
         filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
+          hide_dotfiles = true,
+          hide_gitignored = true,
         },
         follow_current_file = {
           enabled = true,
