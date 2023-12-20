68e3f3e521b50bd0a6072d7b87f4b5a67157f132
Daniel Jonguitud <danieljonguitud17@outlook.com>
2023-12-20 12:01:17 -0500

    adding a space

--- .wezterm.lua
+++ .wezterm.lua
@@ -65,5 +65,6 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
 config.tab_bar_at_bottom = true
+
 -- and finally, return the configuration to wezterm
 return config
