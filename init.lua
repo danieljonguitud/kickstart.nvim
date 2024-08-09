0d9e2c83fcd2162633b7c0415c036b6bce3fe230
danieljonguitud <daniel.jonguitud@sayvero.com>
2024-08-08 19:19:41 -0700

    adding go identation fix

--- init.lua
+++ init.lua
@@ -591,6 +591,15 @@ mason_lspconfig.setup_handlers {
   end,
 }
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+})
+
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
 local cmp = require 'cmp'
